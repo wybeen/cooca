6cf1cbf45c967877dca95662f5d00b9878bc560a
wybeen <wybeen1112@gmail.com>
2019-11-13 08:31:34 +0800

    change to quiz

--- app.py
+++ app.py
@@ -1,32 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import errno
 import os
-import random
+import sys
+import tempfile
+from argparse import ArgumentParser
 
 from flask import Flask, request, abort
 
 from linebot import (
-    LineBotApi, WebhookHandler, WebhookParser
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    LineBotApiError, InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CameraAction, CameraRollAction, LocationAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent, QuickReply, QuickReplyButton
 )
-from pixabay import Image
 
 app = Flask(__name__)
-line_bot_acc_key = os.environ.get('line_bot_acc_key', None)
-if line_bot_acc_key is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-    
-line_bot_set_key = os.environ.get('line_bot_set_key', None)
-if line_bot_set_key is None:
+
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.environ.get('line_bot_set_key', None)
+channel_access_token = os.environ.get('line_bot_acc_key', None)
+if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
 
-line_bot_api = LineBotApi(line_bot_set_key)
-parser = WebhookParser(line_bot_acc_key)
+line_bot_api = LineBotApi(channel_access_token)
+handler = WebhookHandler(channel_secret)
 
 
 @app.route("/callback", methods=['POST'])
@@ -36,54 +55,75 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    print("Request body: " + body)
+    app.logger.info("Request body: " + body)
 
     # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
+    except LineBotApiError as e:
+        print("Got exception from LINE Messaging API: %s\n" % e.message)
+        for m in e.error.details:
+            print("  %s: %s" % (m.property, m.message))
+        print("\n")
     except InvalidSignatureError:
         abort(400)
 
-    imgurl = "https://cdn.hk01.com/di/media/images/2246786/org/3d7a7f543222c5b5f20fe142f2b35e4a.jpg/ikLi2r1cCiuwJpW3Hw7MUk90Ueb6y_TatUNPvLVDT7w"
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            line_bot_api.reply_message(event.reply_token,ImageSendMessage(imgurl, imgurl))
-            continue
-
-        # line_bot_api.reply_message(
-        #    event.reply_token,
-        #    TextSendMessage(text=event.message.text)
-        # )
-        pixabay_key = os.environ.get('pixabay_key')
-        # image operations
-        image = Image(pixabay_key)
-        # custom image search
-        ims = image.search(q=event.message.text,
-            image_type='photo',
-            order='popular',
-            page=1,
-            per_page=20)
-            
-        hits = ims.get("hits")
-        
-        if len(hits) != 0:
-            r = random.randint(0,len(hits))
-            hit = hits[r]
-            previewURL = hit["webformatURL"]            
-            line_bot_api.reply_message(event.reply_token,ImageSendMessage(previewURL, previewURL))
-        else:
-            line_bot_api.reply_message(event.reply_token,ImageSendMessage(imgurl, imgurl))
-            
     return 'OK'
 
 
-# A welcome message to test our server
-@app.route('/')
-def index():
-    return "<h1>Welcome to Cooca !!</h1>"
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+	if (event.reply_token ==  '00000000000000000000000000000000'):
+		return None
+    text = event.message.text
+
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text='Display name: ' + profile.display_name),
+                    TextSendMessage(text='Status message: ' + profile.status_message)
+                ]
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Bot can't use profile API without user ID"))
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    if event.postback.data == 'quiz':
+        results = get('https://opentdb.com/api.php?amount=1&category=22&type=multiple').json()
+        result = results["results"][0]
+        question = result["question"]
+        actionary = []
+        cnt = len(result["incorrect_answers"])
+        r = random.randint(0,cnt)
+        for x in range(cnt)
+            if r==x:
+                actionary.add(MessageAction(label=result["correct_answer"], text='答案是 ' + result["correct_answer"]+', 我答對了, 請給我拍拍手.'))
+            actionary.add(MessageAction(label=result["incorrect_answers"][x], text='答案不是 ' + result["incorrect_answers"][x] + ', 我錯了 orz'))
+        buttons_template = ButtonsTemplate(title=result["category"] + ' - ' + result["difficulty"], text=question, actions=actionary)
+        template_message = TemplateSendMessage(alt_text=question, template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+
 
 if __name__ == "__main__":
-    app.run()
+    # create tmp dir for download content
+    make_static_tmp_dir()
+
+    app.run(debug=True, port=50000, host = '0.0.0.0',ssl_context ='adhoc')
+
+
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
